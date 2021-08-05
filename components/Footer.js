268975d50c46e8704a3c1399661e801da7829e2c
Sachin Bairi <sachinbairi123@gmail.com>
2021-08-05 20:23:48 +0530

    Day-1 Build

---
+++ components/Footer.js
@@ -0,0 +1,54 @@
+function Footer() {
+  return (
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600 md:text-center">
+        <div className="space-y-4 text-xs md:text-sm text-gray-800">
+          <h5 className="font-bold">ABOUT</h5>
+          <p>How Airbnb works</p>
+          <p>Newsroom</p>
+          <p>Investors</p>
+          <p>Airbnb Plus</p>
+          <p>Airbnb Luxe</p>
+          <p>HotelTonight</p>
+          <p>Airbnb for Work</p>
+          <p>Made possible by Hosts</p>
+          <p>Carrers</p>
+          <p>Founders'Letter</p>
+        </div>
+
+        <div className="space-y-4 text-xs md:text-sm text-gray-800">
+          <h5 className="font-bold">COMMUNITY</h5>
+          <p>Diversity & Belonging</p>
+          <p>Accessibility</p>
+          <p>Airbnb Associates</p>
+          <p>Frontline Stays</p>
+          <p>Guest Referrals</p>
+          <p>Airbnb.org</p>
+        </div>
+
+        <div className="space-y-4 text-xs md:text-sm text-gray-800">
+          <h5 className="font-bold">HOST</h5>
+          <p>Host your home</p>
+          <p>Host an Online Experience</p>
+          <p>Host an Experience</p>
+          <p>Responsible hosting</p>
+          <p>Resource Centre</p>
+          <p>Community Centre</p>
+        </div>
+
+        <div className="space-y-4 text-xs md:text-sm text-gray-800">
+          <h5 className="font-bold">SUPPORT</h5>
+          <p>Our COVID-19 Response</p>
+          <p>Help Centre</p>
+          <p>Cancellation options</p>
+          <p>Neighbourhood Support</p>
+          <p>Trust & Safety</p>
+        </div>
+
+      </div>
+        <p className="text-center p-3 text-[1rem] font-bold bg-gray-100 text-gray-600"> &#169; Sachin Airbnb Clone - 2021</p>
+    </div>
+  );
+}
+
+export default Footer;
