b213c42b039c7d33d85730b09c6790bda57b1a06
Sachin Bairi <sachinbairi123@gmail.com>
2021-08-06 13:40:30 +0530

    HeartIcon

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -1,8 +1,28 @@
 import Image from "next/image";
-import { HeartIcon } from "@heroicons/react/outline";
+import { HeartIcon } from "@heroicons/react/solid";
 import { StarIcon } from "@heroicons/react/solid";
+import { useState } from "react";
 
-function InfoCard({ img, location, title, description, star, price, total }) {
+function InfoCard({
+  key,
+  img,
+  location,
+  title,
+  description,
+  star,
+  price,
+  total,
+}) {
+  const flag = false;
+
+  const [heart, setHeart] = useState(flag);
+
+  function toggle() {
+    heart ? setHeart(false) : setHeart(true);
+  }
+
+  console.log(heart);
+
   return (
     <div className="flex flex-col md:flex-row py-7 md:px-2 md:pr-4 border-b cursor-pointer hover:opacity-80 hover:shadow-lg transition duration-200 ease-in-out first:border-t ">
       <div className="relative mb-5 sm:mb-0 h-44 w-screen sm:h-52 sm:w-80 flex-shrink-0">
@@ -17,7 +37,14 @@ function InfoCard({ img, location, title, description, star, price, total }) {
       <div className="flex flex-col flex-grow md:pl-5">
         <div className="flex justify-between">
           <p className="text-sm text-gray-400">{location}</p>
-          <HeartIcon className="h-7 cursor-pointer " />
+
+          <button onClick={toggle}>
+            {heart ? (
+              <HeartIcon className="h-7 cursor-pointer text-[#FD5B61] transition active:translate-y-[-10px] duration-200 ease-in-out" />
+            ) : (
+              <HeartIcon className="h-7 cursor-pointer text-gray-700 transition active:translate-y-[-10px] active:rotate-360  duration-200 ease-in-out" />
+            )}
+          </button>
         </div>
         <h4 className="text-xl">{title}</h4>
         <div className="border-b w-10 pt-2" />
@@ -26,7 +53,8 @@ function InfoCard({ img, location, title, description, star, price, total }) {
 
         <div className="flex justify-between items-end pt-5">
           <p className="flex items-center">
-            <StarIcon className="h-5 text-red-400" />{star}
+            <StarIcon className="h-5 text-red-400" />
+            {star}
           </p>
 
           <div>
